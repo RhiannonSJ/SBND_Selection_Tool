a06d4dcc5ea5f33b81d6c435ce5f95d049f23375
Rhiannon Jones <rjones0211@hotmail.com>
2018-11-06 21:04:35 +0000

    Updated test scripts to work from local machine. Change this to be a configuration variable

--- test/EvdMain.cpp
+++ test/EvdMain.cpp
@@ -44,7 +44,8 @@ int MainTest(){
     std::string name;
     name.clear();
     char file_name[1024];
-    name = "/hepstore/rjones/Samples/FNAL/120918_analysis_sample/11509725_"+std::to_string(i)+"/output_file.root";
+    name = "/home/rhiannon/Samples/LiverpoolSamples/120918_analysis_sample/11509725_"+std::to_string(i)+"/output_file.root";
+    //name = "/hepstore/rjones/Samples/FNAL/120918_analysis_sample/11509725_"+std::to_string(i)+"/output_file.root";
     strcpy( file_name, name.c_str() );
 
     EventSelectionTool::LoadEventList(file_name, events, i);
