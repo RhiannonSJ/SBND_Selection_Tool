9f00ffd93cbc1371c92753be3e65c57e4fe5ae8f
Rhiannon Jones <rjones0211@hotmail.com>
2021-12-14 08:59:05 -0600

    Fixed cvmfs location of the larsoft setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
 source /grid/fermiapp/products/sbnd/setup_sbnd.sh
-source /cvmfs/fermilab.opensciencegrid.org/products/larsoft/setup && retval="$?"
+source /cvmfs/larsoft.opensciencegrid.org/products/setup && retval="$?"
 setup root v6_18_04b -q e17:prof
 cd $SELECTION_TOOL
